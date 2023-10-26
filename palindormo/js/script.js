058ebfb5f1751e4e47cef02fe3bb0c081ed7a0b6
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-26 17:03:22 +0200

    palindrome exercise complete

--- palindormo/js/script.js
+++ palindormo/js/script.js
@@ -1,1 +1,22 @@
+// Chiedo all'utente la prola
+//Dati
+const userWord = prompt("Inserisci una parola che credi sia palindroma");
+// console.log(userWord[userWord.length - 1]);
 
+//Logica
+
+//Definisco la funzione
+function IsItPalindrom(word) {
+  let found = false;
+  if (word[0] === word[word.length - 1]) {
+    found = true;
+  }
+  return found;
+}
+
+//Invoco la funzione
+if (IsItPalindrom(userWord)) {
+  alert("La parola inserita è palindroma");
+} else {
+  alert("La parola inserita non è palindroma");
+}
